a46e0d10320cd6f42f02acdfb5ee8b8d1ec76021
VoldMaire <levenvov@gmail.com>
2017-05-06 01:08:30 +0300

    gitignore test

---
+++ YourTV_WEB/Scripts/modalWin.js
@@ -0,0 +1,12 @@
+﻿
+$(function () {
+    $.ajaxSetup({ cache: false });
+    $("#btn-create-playlist").click(function (e) {
+
+        e.preventDefault();
+        $.get(this.href, function (data) {
+            $('#dialogContent').html(data);
+            $('#playlist-create').modal('show');
+        });
+    });
+});
